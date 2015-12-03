095f274d7e055b87a253ac94962c6a63ea778cb4
takumadx <hirayama.takuma@ohmydigifab.com>
2015-12-03 10:27:22 +0900

    initial commit

---
+++ test/node/testserial.js
@@ -0,0 +1,31 @@
+var EventEmitter = require('events').EventEmitter;
+var UavtalkPacketHandler = require("./uavtalk_packet_handler").UavtalkPacketHandler;
+var UavtalkObjectManager = require("./uavtalk_objman").UavtalkObjectManager;
+var SerialPort = require("serialport").SerialPort;
+
+var cc3d_serial = new SerialPort("/dev/ttyAMA0", {
+	baudrate : 57600
+});
+
+var objMan = UavtalkObjectManager("../uavtalk_json");
+var packetHandler = UavtalkPacketHandler();
+
+cc3d_serial.on("data", packetHandler.unpack(function(packet) {
+	if (!uavtalk_decoder.ready()) {
+		return;
+	}
+	var data = uavtalk_decoder.decode(packet);
+	if (!data) {
+		return;
+	}
+	console.log(data.name);
+	// dataemitter.emit(data.name,data);
+}));
+
+var gtsObj = objMan.getInstance("GCSTelemetryStats");
+var ftsObj = objMan.getInstance("FlightTelemetryStats");
+if (gtsObj.Status == 0) {
+	ftsObj.Status == 1;
+}
+
+cc3d_serial.write(packetHandler.pack(gtsObj));
