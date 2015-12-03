c5a8e0391c76c91b05ddeb0735141bdab25c529a
takumadx <hirayama.takuma@ohmydigifab.com>
2015-12-03 20:04:57 +0900

    refactoring

--- test/node/testserial.js
+++ test/node/testserial.js
@@ -1,9 +1,15 @@
+var async = require('async');
+var Uavtalk = require("uavtalk");
 var EventEmitter = require('events').EventEmitter;
 var SerialPort = require("serialport").SerialPort;
-var Uavtalk = require("uavtalk");
 
-var objMan = Uavtalk.ObjectManager("./openpilot_definitions");
-objMan.init(function() {
+var objMan = new Uavtalk.ObjectManager("./openpilot_definitions");
+
+async.waterfall([ function(callback) {
+	objMan.init(function() {
+		callback();
+	});
+}, function(callback) {
 	var sp = new SerialPort("/dev/ttyAMA0", {
 		baudrate : 57600
 	});
@@ -12,19 +18,26 @@ objMan.init(function() {
 		sp.write(data, function() {
 			sp.drain();
 		});
-	}
+	};
 	sp.on("data", objMan.input_stream);
 	sp.on("open", function() {
-
-		objMan.requestObject("GCSTelemetryStats", function(gtsObj) {
-			console.log(gtsObj);
-			objMan.requestObject("FlightTelemetryStats", function(ftsObj) {
-				console.log(ftsObj);
-				if (ftsObj && ftsObj.Status == 0) {
-					gtsObj.Status == 1;
-					objMan.updateObject(gtsObj);
-				}
-			});
-		});
+		callback();
 	});
+}, function(callback) {
+	objMan.requestObject("GCSTelemetryStats", function(obj) {
+		callback(obj);
+	});
+}, function(callback, gtsObj) {
+	console.log(gtsObj);
+	objMan.requestObject("FlightTelemetryStats", function(obj) {
+		callback(obj);
+	});
+}, function(callback, ftsObj) {
+	console.log(ftsObj);
+	if (ftsObj && ftsObj.Status == 0) {
+		gtsObj.Status == 1;
+		objMan.updateObject(gtsObj);
+	}
+} ], function(err, result) {
+
 });
